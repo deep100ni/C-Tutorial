48156e619a7ba40fe6c517a0db6672c2e9950cf6
deep100ni <deep100ni13@gmail.com>
2024-12-24 16:31:03 +0530

    update C-program2.c

---
+++ C-program2.c
@@ -0,0 +1,14 @@
+/*Divy Raj Soni*/
+//Add 2 numbers 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,sum;
+    clrscr();
+    printf("Enter 2 numbers :");
+    scanf("%d %d",&a,&b);
+    sum = a+b;
+    printf("\n The Sum of %d and %d is %d",a,b,sum);
+    getch();
+}
