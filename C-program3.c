e5240da8bd1171b99c6a8529f52ea5e520d8c063
deep100ni <deep100ni13@gmail.com>
2025-01-03 14:14:50 +0530

    update C-program3.c

---
+++ C-program3.c
@@ -0,0 +1,16 @@
+/*Divy Raj Soni*/
+//Arithmetic Operations 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float a,b;
+    clrscr();
+    printf("Enter two numbers:");
+    scanf("%f %f",&a,&b);
+    printf("\n The Sum of %f and %f is %f",a,b,a+b);
+    printf("\n The Difference of %f and %f is %f",a,b,a-b);
+    printf("\n The Product of %f and %f is %f",a,b,a*b);
+    printf("\n The Division of %f and %f is %f",a,b,a/b);
+    getch();
+}
