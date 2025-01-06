dfd4303b26c45d54b3ea7f21e9eedfa3807a35aa
deep100ni <deep100ni13@gmail.com>
2025-01-06 11:22:05 +0530

    update C-program15.c

---
+++ C-program15.c
@@ -0,0 +1,16 @@
+/*Divy Raj Soni*/
+//Program to calculate gross salary
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int g,b,d,t;
+    clrscr();
+    printf("Enter your salary: ");
+    scanf("%d",&b);
+    t=0.1*b;
+    d=0.12*b;
+    g=b+t+d;
+    printf("\nYour Gross salary = %d",g);
+    getch();
+}
