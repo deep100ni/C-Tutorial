76e58f9c33c47ff8ad7a42645631b361297edf63
deep100ni <deep100ni13@gmail.com>
2025-01-06 10:54:59 +0530

    update C-program13.c

---
+++ C-program13.c
@@ -0,0 +1,14 @@
+/*Divy Raj Soni*/
+//Program to convert temprature in°C to °F
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    float F,C;
+    clrscr();
+    printf("Enter temprature in °C: ");
+    scanf("%f",&C);
+    F=(1.8*C)+32;
+    printf("\n%f°C=%f°F",C,F);
+    getch();
+}
