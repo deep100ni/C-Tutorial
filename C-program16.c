8d079b5c3a4e389aaeb944a93843a8688eaa9302
deep100ni <deep100ni13@gmail.com>
2025-01-07 23:45:16 +0530

    update C-program16.c

---
+++ C-program16.c
@@ -0,0 +1,22 @@
+/*Divy Raj Soni*/
+//Program illustrating use of Shorthand operators
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   
+    int a=10,b;
+    clrscr();
+    b=a;
+    printf("a=10\nb=10");
+    b+=a;
+    printf("\nb=%d after b+=a",b);
+    b-=a;
+    printf("\nb=%d after b-=a",b);
+    b*=a;
+    printf("\nb=%d after b*=a",b);
+    b/=a;
+    printf("\nb=%d after b/=a",b);
+    b%=a;
+    printf("\nb=%d after b%=a",b);
+    getch();
+}
