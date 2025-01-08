94c741d156430d3f314b0e95374776bf756ba6b9
deep100ni <deep100ni13@gmail.com>
2025-01-08 23:55:55 +0530

    update C-program22.c

---
+++ C-program22.c
@@ -0,0 +1,21 @@
+/*Divy Raj Soni*/
+//Program to check whether a number is positive or negative using if-else-if
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int a;
+    clrscr();
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    if(a>0)
+    {        printf("\n%d is a positive number",a);
+    }
+    else if(a<0)
+    {        printf("\n%d is a negative number",a);
+    }
+    else
+    {        printf("\nNumber is equal to 0");
+    }
+    getch();
+}
