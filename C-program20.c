e280fd1f73a527f575590989d45f8c97c9dbdd0e
deep100ni <deep100ni13@gmail.com>
2025-01-08 23:46:22 +0530

    update C-program20.c

---
+++ C-program20.c
@@ -0,0 +1,18 @@
+/*Divy Raj Soni*/
+//Program to compare two numbers using if-else
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int a,b;
+    clrscr();
+    printf("Enter two numbers, a and b : ");
+    scanf("%d %d",&x,&y);
+   if(a>b)
+    {        printf("\na,i.e. ,%d is greater",a);
+     }
+    else
+    {        printf("\nb,i.e. ,%d is greater",b);
+    }
+    getch();
+}
