4fae65b5c6d7802cabfb8c2c1c5e8cccb2c6f78b
deep100ni <deep100ni13@gmail.com>
2025-01-08 23:44:07 +0530

    update C-program19.c

---
+++ C-program19.c
@@ -0,0 +1,18 @@
+/*Divy Raj Soni*/
+//Program to check whether a person is eligible to vote or not
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int age;
+    clrscr();
+    printf("Enter your age (years): ");
+    scanf("%d",&age);
+    if(age>=18)
+    {        printf("\nCongrats! You are eligible to vote.");
+    }
+    else
+    {        printf("\nOops! You are not eligible to vote.");
+    }
+    getch();
+}
