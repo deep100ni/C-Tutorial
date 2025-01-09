77977cbde88afe24346ee066fb80d4152105e6b6
deep100ni <deep100ni13@gmail.com>
2025-01-09 23:39:13 +0530

    update C-program23.c

---
+++ C-program23.c
@@ -0,0 +1,18 @@
+/*Divy Raj Soni*/
+//Program to check whether a number is even or odd using if-else
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int a;
+    clrscr();
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    if(a%2==0)
+    {        printf("\n%d is a even number",a);
+    }
+    else
+    {        printf("\n%d is a odd number",a);
+    }
+    getch();
+}
