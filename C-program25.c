750d75425666294e7ae33ffa3a98fc206073c634
deep100ni <deep100ni13@gmail.com>
2025-01-09 23:43:21 +0530

    update C-program25.c

---
+++ C-program25.c
@@ -0,0 +1,24 @@
+/*Divy Raj Soni*/
+//Program to compare numbers using relational operators (II)
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int a,b;
+    clrscr();
+    printf("Enter two numbers, a and b: ");
+    scanf("%d %d",&a,&b);
+    printf("\n(%d==%d) is ",a,b);
+    a==b?printf("True"):printf("False");
+    printf("\n(%d!=%d) is ",a,b);
+    a!=b?printf("True"):printf("False");
+    printf("\n(%d<%d) is ",a,b);
+    a<b?printf("True"):printf("False");
+    printf("\n(%d>%d) is ",a,b);     
+    a>b?printf("True"):printf("False");
+    printf("\n(%d<=%d) is ",a,b);
+    a<=b?printf("True"):printf("False");
+    printf("\n(%d>=%d) is ",a,b);
+    a>=b?printf("True"):printf("False");
+    getch();
+}
