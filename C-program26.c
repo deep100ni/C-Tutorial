5e04f1ad5204da15a7c8de9b6d918a35698b70ba
deep100ni <deep100ni13@gmail.com>
2025-01-10 23:23:15 +0530

    update C-program26.c

---
+++ C-program26.c
@@ -0,0 +1,20 @@
+/*Divy Raj Soni*/
+//Using Logical operators
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   
+    int a,b;
+    clrscr();
+    printf("Enter two numbers, a and b: ")
+    scanf("%d %d",&a,&b);
+    printf("\n(%d>=%d)&&(%d==%d) is ",a,b,a,b);
+    (a>=b)&&(a==b)?printf("True"):printf("False");
+    printf("\n(%d>=%d)||(%d==%d) is ",a,b,a,b);
+    (a>=b)||(a==b)?printf("True"):printf("False");
+    printf("\n(%d<=%d)&&(%d==%d) is ",a,b,a,b);
+    (a<=b)&&(a==b)?printf("True"):printf("False");
+    printf("\n!(%d==%d) is ",a,b,a,b);
+    !(a==b)?printf("True"):printf("False");
+    getch();
+}
