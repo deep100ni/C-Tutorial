b946540a05ec19a90b97239db3403ec2aaf516c0
deep100ni <deep100ni13@gmail.com>
2025-01-11 23:56:26 +0530

    update C-program30.c

---
+++ C-program30.c
@@ -0,0 +1,26 @@
+/Divy Raj Soni*/
+//Using Switch Case (Vowels check)
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    char alpha;
+    clrscr();
+    printf("Enter a character to check if it is vowel or not (Plz enter A-Z or a-z only): ");
+    scanf("%c",&alpha);
+    switch(alpha)
+     {          
+      case 'a': 
+      case 'e': 
+      case 'i': 
+      case 'o': 
+      case 'u': 
+      case 'A': 
+      case 'E': 
+      case 'I': 
+      case 'O': 
+      case 'U': printf("It's a vowel"); break;
+      default: printf("It's a consonant");
+    }
+    getch();
+  } 
