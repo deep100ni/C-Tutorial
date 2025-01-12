95dcde33235980d4bd3439b6c12504055f5278c5
deep100ni <deep100ni13@gmail.com>
2025-01-12 23:31:08 +0530

    update C-program33.c

---
+++ C-program33.c
@@ -0,0 +1,16 @@
+/*Divy Raj Soni*/
+//Using While Loop (20-30 printing)
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    int a,b;
+    clrscr();
+    printf("Enter initial and final number: ");
+    scanf("%d %d",&a,&b);
+    while(a<=b)
+    {        printf("%d\n",a);
+        a++;
+    }
+    getch();
+}
