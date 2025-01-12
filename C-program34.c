f9f5b6f1259ba95d7169d5ea3717b922c551591b
deep100ni <deep100ni13@gmail.com>
2025-01-12 23:34:37 +0530

    update C-program34.c

---
+++ C-program34.c
@@ -0,0 +1,18 @@
+/*Divy Raj Soni*/
+//Using Do While Loop (series printing)
+#include<stdio.h>
+#include<conio.h>
+void main()
+{   
+    int a,b;
+    clrscr();
+    printf("Enter initial and final number: ");
+    scanf("%d %d",&a,&b);
+    do
+    {        
+        printf("%d\n",a);
+            a++;
+    }
+    while(a<=b);
+    getch();
+}
